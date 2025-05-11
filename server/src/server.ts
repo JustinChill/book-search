e4bbaab0297b7d6fe02ea1050b792636ad53f003
Justin Hill <justinrichardhill@gmail.com>
2025-05-10 23:03:29 -0400

    Fix error: ENOENT

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,12 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist'))); // Adjusted for Vite build output
-  app.get('*', (_req, res) => { // Prefixed req with an underscore
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist'))); // Adjusted for Vite build output
+//   app.get('*', (_req, res) => { // Prefixed req with an underscore
+//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//   });
+// }
 
 const startApolloServer = async () => {
   await server.start();
